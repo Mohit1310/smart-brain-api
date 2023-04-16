77883fede6f11cfec4fee15e2eec328e7053ad76
Mohit Dayma <70746260+Mohit1310@users.noreply.github.com>
2023-04-16 09:01:54 +0530

    testing changes

--- server.js
+++ server.js
@@ -27,7 +27,9 @@ app.use(cors())
 
 app.get("/", (req, res) => { res.send('success'); });
 app.post("/signin", (signin.handleSignin(db, bcrypt)))
-app.post("/register", (req, res) => { register.handleRegister(req, res, db, bcrypt) });
+app.post("/register", (req, res) => { 
+  res.send('success register');
+  register.handleRegister(req, res, db, bcrypt) });
 app.get("/profile/:id", (req, res) => { profile.handleProfileGet(req, res, db) });
 app.put("/image", (req, res) => { image.handleImage(req, res, db) });
 app.post("/imageurl", (req, res) => { image.handleApiCall(req, res) })
